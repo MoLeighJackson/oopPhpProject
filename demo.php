4b5c6738a3955194ad5c648510d02c003cc29358
Mo Leigh <moleighjackson@gmail.com>
2017-04-04 11:10:01 -0400

    Update demo.php

--- demo.php
+++ demo.php
@@ -6,6 +6,15 @@ require 'class.Language.inc';
 echo '<h2>Instantiating Language</h2>';
 $language = new Language;
 
+
 // inspect contents of object using var dump
 echo '<h2>Blank Language</h2>';
 echo '<tt><pre>' . var_export($language, TRUE) . '</pre></tt>';
+
+echo '<h2>Setting properties...</h2>';
+  $language->language_name = 'French';
+  $language->helloworld_text = 'Bonjour le monde';
+  echo '<tt><pre>' . var_export($language, TRUE) . '</pre></tt>';
+
+echo '<h2>Displaying language and hello world in that language...</h2>';
+echo $language->display();
