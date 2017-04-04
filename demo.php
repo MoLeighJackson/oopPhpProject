513be1aa6f451bc82d6074c6ec218a5576f3d334
Mo Leigh <moleighjackson@gmail.com>
2017-04-04 17:50:48 -0400

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,20 +1,25 @@
-<!--instantiating language object, manipulate properties, debug contents, execute method-->
+<!--instantiate language object,
+change properties,
+debug contents (using var dump),
+execute method-->
 
 <?php
 require 'class.Language.inc';
 
-echo '<h2>Instantiating Language</h2>';
+echo '<h2>Instantiating an Object</h2>';
 $language = new Language;
 
 
 // inspect contents of object using var dump
-echo '<h2>Blank Language</h2>';
+echo '<h2>Object - Language</h2>';
 echo '<tt><pre>' . var_export($language, TRUE) . '</pre></tt>';
 
-echo '<h2>Setting properties...</h2>';
+echo '<h2>Properties</h2>';
   $language->language_name = 'French';
-  $language->helloworld_text = 'Bonjour le monde';
+  $language->helloworld_text = 'Bonjour le monde!';
+  $language->helloworld_eng = 'Hello, World!';
   echo '<tt><pre>' . var_export($language, TRUE) . '</pre></tt>';
 
-echo '<h2>Displaying language and hello world in that language...</h2>';
+echo '<h2>Display language, <strong>Hello World</strong> in that language,
+        followed by <strong>Hello World</strong> in <em>English</em></h2>';
 echo $language->display();
